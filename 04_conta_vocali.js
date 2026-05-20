ce87125d48713d9e0d4407121af85d504fd9e567
thomaslazzeri3 <thomaslazzeri3@gmail.com>
2026-05-20 23:03:51 +0200

    almost done exercise 4 there are not the (a, a, i)

--- 04_conta_vocali.js
+++ 04_conta_vocali.js
@@ -4,10 +4,29 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function numberVocals(str) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u') {
+            count++;
+        }
+    }
+    return count;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(numberVocals(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+// Esercizio con arrow function
+const numberVocals2 = (str) => {
+    let count2 = 0;
+    for (let i = 0; i > str.length; i++) {
+        if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u') {
+            count2++;
+        }
+    }
+    return count2;
+}
+console.log(numberVocals2(word));
