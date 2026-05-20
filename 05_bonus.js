f97e2ee7085261d5b20a94a40e6b84198cea78eb
thomaslazzeri3 <thomaslazzeri3@gmail.com>
2026-05-20 23:06:48 +0200

    done exercise 5

--- 05_bonus.js
+++ 05_bonus.js
@@ -9,10 +9,33 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function greeting(person) {
+    const hour = 0;
+    if (hour < 13) {
+        return "Buongiorno " + person + ".";
+    } else if (hour < 17) {
+        return "Buon pomeriggio " + person + ".";
+    } else {
+        return "Buonasera " + person + ".";
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(greeting(name));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+
+// Esercizio con arrow function
+const greeting2 = (person) => {
+    const hour2 = 0;
+    if (hour2 < 13) {
+        return "Buongiorno " + person + ".";
+    }
+    else if (hour2 < 17) {
+        return "Buon pomeriggio " + person + ".";
+    }
+    else {
+        return "Buonasera " + person + ".";
+    }
+}
+console.log(greeting2(name));
